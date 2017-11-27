b1f7e31d5f0c346de22aff55cfa1f1d7c8956ac0
方元杰 <yuanjie.fang@tpadsz.cn>
2017-11-27 13:26:23 +0800

    springboot分布式事务控制+缓存控制+定时任务+打包部署

--- src/main/java/com/imooc/controller/UserController.java
+++ src/main/java/com/imooc/controller/UserController.java
@@ -4,6 +4,7 @@ import com.imooc.test01.mapper.UserMapper01;
 import com.imooc.test01.service.UserService01;
 import com.imooc.test02.service.UserService02;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,9 @@ public class UserController {
     @Autowired
     private CacheManager cacheManager;
 
+    @Value("${name}")//获取自定义参数的注解
+    private String name;
+
     @RequestMapping("/addUser")
     @Transactional
     public String add() {
@@ -41,7 +45,21 @@ public class UserController {
 
     //测试将数据从cache缓存中删除
     @RequestMapping("/removeCache")
-    public void remove(){
+    public void remove() {
         cacheManager.getCache("baseCache").clear();
     }
+
+    //测试异步执行注解 @Async
+    @RequestMapping("/sendMsg")
+    public void sendMsg() {
+        System.out.println("#sendMsg  1");
+        userService01.sendMsg();
+        System.out.println("#sendMsg  4");
+    }
+
+    @RequestMapping("/getName")
+    public String getName() {
+        return name;
+    }
+
 }
--- src/main/java/com/imooc/test01/service/UserService01.java
+++ src/main/java/com/imooc/test01/service/UserService01.java
@@ -2,6 +2,7 @@ package com.imooc.test01.service;
 
 import com.imooc.test01.mapper.UserMapper01;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,5 +15,19 @@ public class UserService01 {
 
     public int addUser(String name, Integer age) {
         return userMapper01.addUser(name, age);
+    }
+
+    @Async
+    public void sendMsg() {
+        System.out.println("#sendMsg 开始  2");
+        for (int i = 0; i < 5; i++) {
+            try {
+                System.out.println("i:" + i);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("#sendMsg 结束  3");
     }
 }
